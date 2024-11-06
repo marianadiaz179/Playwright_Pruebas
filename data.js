34e18c78ae0d072ba92baa41ee077f025be95812
Mariana Díaz <m.diaza2@uniandes.edu.co>
2024-11-05 21:32:47 -0500

    Initial code

---
+++ data.js
@@ -0,0 +1,11 @@
+module.exports = {
+    login: {
+      username: 'm.diaza2@uniandes.edu.co',
+      password: 'M4r14n417*'
+    },
+    post: {
+      title: 'Mi nueva publicación',
+      content: 'Este es el contenido de la publicación.'
+    }
+  };
+  
